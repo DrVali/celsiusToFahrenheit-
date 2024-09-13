b731ac09dd4065703203afca889aae1e8793f911
DrVali <ahmadi.world@gmail.com>
2024-09-13 23:53:06 +0330

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,31 @@
+'use strict';
+function celsiusToFahrenheit(){
+   const celsiusTemperature  =document.getElementById("temperature").value;
+
+   let Fahrenheit ;
+
+  if(!isNaN(celsiusTemperature) ){
+           Fahrenheit = (1.8 * celsiusTemperature ) + 32;
+          alert(  "The teprature in Farenheit is "  + Fahrenheit);
+
+
+  }
+  else{
+         alert( "please insert number in both of text box.");
+  }
+
+}
+
+     /*    راه دوم      */
+  /*
+let celsiusTemperature =10;
+    let Fahrenheit1 ;
+
+ function celsiusToFahrenheit(celsiusTemperature){
+     Fahrenheit1 = (1.8 * celsiusTemperature ) + 32;
+          alert(  "The teprature in Farenheit is "  + Fahrenheit1);
+ }
+
+ celsiusToFahrenheit(celsiusTemperature);
+
+                     */
